cb5ba6155fa7ef8856713c65e21a4c9d2772c77c
”LynxTi” <andreytimoshenko92@gmail.com>
2021-08-07 15:02:20 +0300

    eror

--- bin/runners/web/wsRunner.js
+++ bin/runners/web/wsRunner.js
@@ -1,41 +1,20 @@
 const WebSocketServer =  require('ws');
 const sio = require('socket.io'); 
-const broadcastChannel = require('broadcast-channel')
  /**
   * Get port from environment and store in Express.
   */
  const run = (httpServer) => {
-    let htmlhMeseges ='';
-
     const io = sio(httpServer);
 
     io.on('connection', (socket)=> {
+        // let inform = null;
+        socket.on('/chat', (data)=> {
+            socket.broadcast.emit('/chat', data );
+        });
 
-        socket.on('/chat', (data, cb)=> {
-            console.log('data: ', data);
-            const { usName, userMesege } = data;
-
-            htmlhMeseges += `<div>${usName}:<div>
-                <div>${userMesege}</div>
-                <br>`;
-                console.log(htmlhMeseges);
-                cb(htmlhMeseges);
-        })
         socket.on('disconnect', () => {
             // asd
-        })
-    })
-        //web Server
-//     const wss =  new WebSocketServer.Server( {server: server} );
-//     wss.on('connection', (ws) => {
-//     console.log('bac: web socket ON');
-
-//     ws.on('message', (message)=> {
-//     console.log('Front send: ', message);
-
-//     });
-
-//     ws.send('blabla')
-// });
+        });
+    });
 }
 module.exports = run;
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,17 @@
-const scoket = io();
+// const { socket } = require("socket.io");
+const socket = io();
 
 const userLoginForm = document.querySelector('.loginForm');
 const usersMesegesBlock = document.querySelector('.usersMeseges');
-const formIserMesege = document.querySelector('.usersMeseges');
 const formUserMesege = document.querySelector('.formUserMesege');
+const chatBlock = document.querySelector('.chatBlock');
+
+const addNewMesege = (userName, userMesege) => {
+    const html = `<div>${userName}: ${userMesege}`;
+    usersMesegesBlock.insertAdjacentHTML('beforeend', html); 
+}
 
 let userName;
-// const chatChannel = new BroadcastChannel('chatChannel');
 
 userLoginForm.addEventListener('submit', (ev)=> {
     ev.preventDefault();
@@ -14,7 +19,8 @@ userLoginForm.addEventListener('submit', (ev)=> {
     const formData = new FormData(ev.target);
     userName = formData.get('userName');
 
-    console.log(userName);
+    userLoginForm.classList.remove('showBlock');
+    chatBlock.classList.add('showBlock');
 });
 
 formUserMesege.addEventListener('submit', (ev) => {
@@ -24,13 +30,13 @@ formUserMesege.addEventListener('submit', (ev) => {
     const userMesege = formData.get('userMesege');
 
         if (userMesege !== '') {
-            scoket.emit('/chat', {usName: userName, userMesege: userMesege}, (data) => {
-                usersMesegesBlock.innerHTML = data
-            });
-            // chatChannel.postMessage('HEY');
-            // chatChannel.onmessage = (ev) => {
-            //     console.log(ev.data);
-            // }
+            socket.emit('/chat', { userName: userName, userMesege: userMesege });
+            addNewMesege(userName, userMesege);
         }
+
+        socket.on(`/chat`, (data) => {
+            const { userName, userMesege } = data;
+            addNewMesege(userName, userMesege);
+        });
 })
 
